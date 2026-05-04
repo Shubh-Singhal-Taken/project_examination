c9cce536119db55765c8c1ccbc10c90b6b0bccef
Shubh Singhal <guptashubh010@gmail.com>
2026-05-04 09:50:24 +0530

    Version 1 Fully Developed

--- routes/auth.js
+++ routes/auth.js
@@ -1,65 +1,65 @@
 const express = require('express');
-const router = express.Router();
-const passport = require('passport');
+const router  = express.Router();
+const passport = require('passport')
 const User = require('../models/user');
 
 
-router.get('/register', (req, res) => {
+router.get('/register',(req,res)=>{
     res.render('auth/register')
 });
 
 
-router.post('/register', async (req, res, next) => {
-    try {
-        var username = req.body.username
+router.post('/register',async(req,res,next)=>{
+  try{
+        var username         = req.body.username
         var password = req.body.password
         var nationality = req.body.nationality
-        var travelStyle = req.body.travelStyle
+        var travelStyle   = req.body.travelStyle
         var favoriteContinent = req.body.favoriteContinent
 
-        var newUser = new User({username: username, nationality: nationality, travelStyle: travelStyle, favoriteContinent: favoriteContinent})
-        var registeredUser = await User.register(newUser, password)
+        var newUser = new User({username:username,nationality:nationality,travelStyle:travelStyle,favoriteContinent:favoriteContinent})
+        var registeredUser= await User.register(newUser,password)
         console.log(registeredUser)
-        req.login(registeredUser, function(err) {
-            if (err) {
+        req.login(registeredUser,function(err){
+          if(err){
                 console.log(err)
                 return next(err)
             }
-            req.flash('success', 'Welcome, ' + registeredUser.username + '!')
+            req.flash('success','Welcome, '+registeredUser.username+'!')
             res.redirect('/journals')
         })
-    } catch (e) {
-        console.log("register error: " + e)
-        req.flash('error', e.message)
+    }catch(e){
+    console.log("register error: "+e)
+        req.flash('error',e.message)
         res.redirect('/register')
     }
 });
 
 
-router.get('/login', (req, res) => {
+router.get('/login',(req,res)=>{
     res.render('auth/login')
 });
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), function(req, res) {
-    var user = req.user
-    var name = user.username
-    console.log(name + " logged in")
-    req.flash('success', 'Welcome back, ' + name + '!')
+router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),function(req,res){
+    var user=req.user
+    var name=user.username
+    console.log(name+" logged in")
+    req.flash('success',"Welcome back, "+name+"!")
     res.redirect('/journals')
 });
 
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) {
+router.get('/logout',(req,res,next)=>{
+    req.logout(function(err){
+      if(err){
             console.log(err)
             return next(err)
         }
         console.log("user logged out")
-        req.flash('success', 'Logged out successfully.')
+        req.flash('success','Logged out successfully.')
         res.redirect('/login')
     })
 });
 
 
-module.exports = router;
+module.exports=router;
